8586760fc3fbfe15dba82ca2fb92d9f8a62aa83f
yeonsssu26 <monica0326@khu.ac.kr>
2021-05-06 20:05:04 +0900

    연수성공

--- dropbox/views.py
+++ dropbox/views.py
@@ -5,8 +5,8 @@ class DummyView(APIView):
     def get(self, request):
         print(request)
         return Response({
-            "message": "경희대학교 2021-1 클라우드 컴퓨팅 드롭박스 프로젝트에 오신 것을 환영합니당 ^_^ 진수 배포 성공",
-            "confirmed_teammates": ["박진수", "어피치"]
+            "message": "경희대학교 2021-1 클라우드 컴퓨팅 드롭박스 프로젝트에 오신 것을 환영합니당 ^_^ 연수 배포 성공",
+            "confirmed_teammates": ["김연수", "어피치"]
         })
 
 
