9e46eb4e54d2a314d34b265e314ef590f7bdac0c
이재용 <jae_young_97@khu.ac.kr>
2021-05-06 23:32:59 +0900

    upload jaeyong confirmed_teammates

--- dropbox/views.py
+++ dropbox/views.py
@@ -6,7 +6,7 @@ class DummyView(APIView):
         print(request)
         return Response({
             "message": "경희대학교 2021-1 클라우드 컴퓨팅 드롭박스 프로젝트에 오신 것을 환영합니당 ^_^ 연수 배포 성공",
-            "confirmed_teammates": ["김연수", "어피치"]
+            "confirmed_teammates": ["김연수", "어피치"], ["이재용","삼성"]
         })
 
 
