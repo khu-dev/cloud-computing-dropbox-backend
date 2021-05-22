49859a4f39409701d58fa56387d881bee129beab
JungByungGi <qudrlwjd12@naver.com>
2021-05-22 22:03:53 +0900

    ENH: create user register class function

---
+++ user/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+
+from django.contrib.auth.models import User
+
+
+# 유효성 검사 추가하여 비밀번호와 이메일 추가 시 유효한 비밀번호, 이메일인지 체크하도록 구현
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]  # 사용자 이메일을 유니크 필드로 지정
+    )
+
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    check_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        # 장고에서 제공하는 auth_user 모델 사용
+        model = User
+        fields = ('username', 'password', 'check_password', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    # 비밀번호 일치 유효성 검사
+    def validate(self, attrs):
+        if attrs['password'] != attrs['check_password']:
+            raise serializers.ValidationError({"password": "비밀번호가 일치하지 않습니다."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
---
+++ user/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from .serializers import RegisterSerializer
+
+
+# 회원 가입
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
