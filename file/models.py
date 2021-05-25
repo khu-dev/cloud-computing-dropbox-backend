59890e8637d5aa537be8026ef94317c4a5e9e816
JungByungGi <qudrlwjd12@naver.com>
2021-05-25 16:05:06 +0900

    ENH : 파일 모델 생성

--- file/models.py
+++ file/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 
+# 파일 모델
 class File(models.Model):
+    id = models.TextField(primary_key=True)
+    register_date = models.DateTimeField(auto_now_add=True) # 최초 등록일
+    modified_date = models.DateTimeField(auto_now=True, null=True) # 수정 날짜
+    is_shared = models.BooleanField(default=False)
     file = models.FileField(upload_to='uploaded')
-    filename = models.FileField()
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column="user_id")  #username이 아닌 user 모델의 id(pk)를 foreignkey로 하여 username 값을 가져오고 싶을 때는 참조하기
+
+    class Meta:
+        db_table = "DROPBOX_FILE"
