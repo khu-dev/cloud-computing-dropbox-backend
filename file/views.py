aa9ef6c183746d21bef78bbf0a80cec843ccf042
yeonsssu26 <monica0326@khu.ac.kr>
2021-05-26 10:29:30 +0900

    BUG : file/vies.py > django.core.exceptions.ImproperlyConfigured error

--- file/views.py
+++ file/views.py
@@ -1,31 +1,67 @@
-from django.shortcuts import render
-
-# Create your views here.
+# DRF
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet, ModelViewSet
-from django.core.files.storage import default_storage
+from rest_framework.viewsets import ModelViewSet
+from django.http import HttpResponse, Http404
 
+# User
+from django.contrib.auth.models import User
+
+# File
 from file.models import File
 from file.serializers import FileSerializer
 
+# path
+import os
+
+# django.core.exceptions.ImproperlyConfigured error
+# from django.apps import AppConfig
+# AppConfig.default = False
 
 class FileViewSet(ModelViewSet):
     serializer_class = FileSerializer
+
     def get_queryset(self):
         return File.objects.all()
+
+    # upload files
     def create(self, request, *args, **kwargs):
-        # print(request.FILES)
-        # file = request.FILES['files']
-        # print('Upload', file)
-        # print('Upload', dir(file))
-        # for f = File(file=file)
-        # f.save()
-        for file in request.FILES.getlist('files'):
-            print('Upload', file)
-            print('Upload', dir(file))
-            f = File(file=file)
-            f.save()
+        # check user
+        user = User.objects.filter(User.get_username(self))
+        if user in User.objects.all():
+
+            # upload files
+            for file in request.FILES.getlist('files'):
+                print('Upload', file)
+                print('Upload', dir(file))
+
+                f = File(file=file)
+                f.save()
+
+                f.file.name = os.path.join(user, f.file.name)
+                print(f.file.path)
 
         return Response({'message': 'ok'}, status=status.HTTP_201_CREATED)
 
+
+    # get each user's file list
+    def list(self, request, *args, **kwargs):
+        # check user
+        user = User.objects.filter(User.get_username(self))
+        if user in User.objects.all():
+
+            # return file list
+            print(request.FILES.getlist("files"))
+            return super().list(request, args, kwargs)
+
+
+    # download file
+    # https://stackoverflow.com/questions/43441882/download-a-file-with-django
+    def download(self, request):
+        file = File.objects.filter(File.get_id())
+        if file in File.objects.all():
+            print("Download", file)
+            print("Download", dir(file))
+            file = File(file=file)
+            return HttpResponse(file, content_type='text/plain')
+        raise Http404
